91de0a37292a19c77b5b540c1c0ce73968f9277d
Zeus Gómez Marmolejo <zeus@aluzina.org>
2010-10-02 11:12:23 +0200

    Removed unused cores

--- cores/yadmc/sim/runsim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cver yadmc_test.v mt48lc16m16a2.v ../../rtl/yadmc/yadmc_dpram.v ../../rtl/yadmc/yadmc_spram.v ../../rtl/yadmc/yadmc_sync.v ../../rtl/yadmc/yadmc_sdram16.v ../../rtl/yadmc/yadmc.v
-
-#iverilog -o sim yadmc_test.v mt48lc16m16a2.v ../../rtl/yadmc/yadmc_dpram.v ../../rtl/yadmc/yadmc_spram.v ../../rtl/yadmc/yadmc_sync.v ../../rtl/yadmc/yadmc_sdram16.v ../../rtl/yadmc/yadmc.v
-#./sim
--- cores/yadmc/sim/t.do
+++
@@ -1,7 +0,0 @@
-quit -sim
-vdel -all -lib work
-vlib work
-vlog -work work -lint ../rtl/yadmc_dpram.v ../rtl/yadmc_sdram16.v ../rtl/yadmc_spram.v ../rtl/yadmc_sync.v ../rtl/yadmc.v mt48lc16m16a2.v yadmc_test.v
-vsim -L /opt/Xilinx/10.1/modelsim/verilog/unisims -novopt -t ps work.yadmc_test
-add wave -radix hexadecimal -r /*
-
